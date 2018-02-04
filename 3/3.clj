5aa7f021dbf74475209fc283fafbd30e24bb5dd5
Ilmo Raunio <ilmoraunio@users.noreply.github.com>
2018-02-04 23:28:38 +0200

    Full working implementation for part I

--- 3/3.clj
+++ 3/3.clj
@@ -1,6 +1,4 @@
-;; determining the number of elements in an edge, given any value
-
-(defn get-elements-in-edge [square]
+(defn edge-count [square]
   (int (let [sqrt (Math/sqrt square)]
          (cond
            (and (= (mod sqrt 1) 0.0) 
@@ -9,25 +7,31 @@
            :else                             (+ (Math/floor sqrt) 1)))))
 
 (defn center-line-squares [square]
-  (let [n      (get-elements-in-edge square)
-        step   (dec (get-elements-in-edge square))
-        prefix (dec (/ (dec n) 2))
-        start-square 2]
+  (let [step              (dec (edge-count square))
+        prefix            (dec (/ (dec (edge-count square)) 2))
+        start-square      (int (inc (Math/pow (- (edge-count square) 2) 2)))]
     [(+ prefix start-square)
      (+ prefix start-square step)
      (+ prefix start-square step step)
      (+ prefix start-square step step step)]))
 
-(defn get-distance-to-center-line [square]
+(defn distance-to-center-line [square]
   ;; determine the elements-in-edge
-  ;; calculate the center line square values for that layer (ie. [2, 4, 6, 8] when get-elements-in-edge is 3)
+  ;; calculate the center line square values for that layer (ie. [2, 4, 6, 8] when edge-count is 3)
   ;; compare square against center line values and determine nearest candidate
   ;; calculate steps
-  (let [n (get-elements-in-edge square)]
-    ))
+  (let [distances (->> (center-line-squares square)
+                       (map #(- % square))
+                       (map #(Math/abs %)))]
+    ;; Some maximum values in distances will not reflect the truth
+    ;; (for instance a 10 has only 3 steps to 23, but is shown as 13)
+    ;; luckily we only care about the minimum value, which means we can
+    ;; discard the rest of values in distances.
+    (reduce min distances)))
 
-(defn get-distance-from-center-line-to-square-one [square]
-  ;; determine the elements-in-edge
-  ;; subtract elements-in-edge by one and divide by 2 -> how many steps it takes to get to square value 1
-  (let [n (get-elements-in-edge square)]
+(defn distance-from-center-line-to-square-one [square]
+  (let [n (edge-count square)]
     (/ (dec n) 2)))
+
+(defn manhattan-distance-of-spiral-memory [square]
+  (+ (distance-to-center-line square) (distance-from-center-line-to-square-one square)))
