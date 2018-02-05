60b10a2119190aae07807ebb86d7eee3f46559af
Ilmo Raunio <ilmoraunio@users.noreply.github.com>
2018-02-05 11:03:52 +0200

    Add actual content for passphrases symbol

--- 4/4.clj
+++ 4/4.clj
@@ -1,5 +1,5 @@
 (defn valid-passphrase? [passphrase]
   (every? true? (map (fn [[_ frequency]] (= frequency 1)) (frequencies (clojure.string/split passphrase #"\s")))))
 
-(def passphrases (comment "todo") [])
+(def passphrases (clojure.string/split (slurp "passphrases.txt") #"\n"))
 (->> passphrases (map valid-passphrase?) frequencies)
