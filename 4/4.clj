7e8ae481cae9867c7733b07359a7e83bd720ec6b
Ilmo Raunio <ilmoraunio@users.noreply.github.com>
2018-02-05 11:57:49 +0200

    Solution for day 4, part II

--- 4/4.clj
+++ 4/4.clj
@@ -3,3 +3,11 @@
 
 (def passphrases (clojure.string/split (slurp "passphrases.txt") #"\n"))
 (->> passphrases (map valid-passphrase?) frequencies)
+
+;; part II
+
+(defn valid-passphrase-part-II? [passphrase]
+  (every? true? (map (fn [[_ frequency]] (= frequency 1)) 
+                     (frequencies (map frequencies (clojure.string/split passphrase #"\s"))))))
+
+(->> passphrases (map valid-passphrase-part-II?) frequencies)
